5ecbbf167c86870912d21887f9fd468b66aca33d
beemaster <ugor.finno@yandex.ru>
2025-01-05 07:18:05 +0500

    create module 10 hw 4 Queues for data exchange between threads

--- module_10_4.py
+++ module_10_4.py
@@ -1,4 +1,4 @@
-from queue import Queue, LifoQueue
+from queue import Queue
 from random import randint
 from threading import Thread
 from time import sleep
@@ -18,22 +18,22 @@ class Guest(Thread):
         self.name = name
 
     def run(self):
-        sleep(randint(3,10))
+        sleep(randint(3, 10))
 
 
 class Cafe:
 
     def __init__(self, *args, **kwargs):
-        self.tables = {table.number:table for table in args if isinstance(table,Table)}
+        self.tables = {table.number: table for table in args if isinstance(table, Table)}
         self.tables_number = len(self.tables)
         self.empty_tables = Queue(self.tables_number)
         for table_number in self.tables.keys():
             self.empty_tables.put(table_number)
         self.queue = Queue()
 
-    def guest_arrival(self,*guests):
+    def guest_arrival(self, *guests):
         for guest in guests:
-            if not isinstance(guest,Guest):
+            if not isinstance(guest, Guest):
                 continue
             if self.empty_tables.empty():
                 self.queue.put(guest)
@@ -42,31 +42,25 @@ class Cafe:
             empty_table = self.empty_tables.get()
             self.tables[empty_table].guest = guest
             print(f'{guest.name} сел(-а) за стол номер {empty_table}')
-            print(f'{self.empty_tables.qsize()} свободных столов осталось')
+            print(f'{self.empty_tables.qsize()} свободных столов осталось. Очередь из {self.queue.qsize()} человек(-a)')
             guest.start()
 
     def discuss_guests(self):
-        while not self.queue.empty() or self.empty_tables.qsize()<self.tables_number:
-            for table_number, table in self.tables.items():
+        while not self.queue.empty() or self.empty_tables.qsize() < self.tables_number:
+            for table in self.tables.values():
                 if table.guest is None:
                     if not self.queue.empty():
                         guest = self.queue.get()
+                        table_number = self.empty_tables.get()
                         self.tables[table_number].guest = guest
                         guest.start()
-                        print(f'{guest.name} сел(-а) за стол номер {table_number}')
-                        print(f'{self.empty_tables.qsize()} свободных столов осталось')
-                    else:
-                        break
+                        print(f'{guest.name} вышел(-шла) из очереди и сел(-а) за стол номер {table_number}')
                     continue
                 if not table.guest.is_alive():
                     print(f"{table.guest.name} покушал(-а) и ушёл(ушла)")
-                    print(f"Стол номер {table_number} свободен")
-                    self.tables[table_number].guest = None
-                    self.empty_tables.put(table_number)
-            else:
-                break
-
-
+                    print(f"Стол номер {table.number} свободен")
+                    self.tables[table.number].guest = None
+                    self.empty_tables.put(table.number)
 
 if __name__ == '__main__':
     tables = [Table(number) for number in range(1, 6)]
@@ -79,7 +73,6 @@ if __name__ == '__main__':
     guests = [Guest(name) for name in guests_names]
     # Заполнение кафе столами
     cafe = Cafe(*tables)
-    # print(cafe.empty_tables.qsize())
     cafe.guest_arrival(*guests)
-    # print(cafe.empty_tables.qsize())
     cafe.discuss_guests()
+    print(f'В очереди в кафе {cafe.queue.qsize()} человек(-а)')
