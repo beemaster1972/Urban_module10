abf11299b2d43c42a664e932db41054735cfedcb
Дмитрий Янно <ugor.finno@yandex.ru>
2025-01-15 17:14:30 +0500

    modify module 10 hw 6 Introspection

--- module_10_6.py
+++ module_10_6.py
@@ -1,11 +1,19 @@
+import requests
+
+
 def introspection_info(obj)->dict:
     result={}
-    # print(obj.__name__)
+    info = dir(obj)
+    if '__name__' in info:
+        print(obj.__name__)
     print(type(obj))
-    print(dir(obj))
-    print(obj.__doc__)
+    print(info)
+    print(obj.__dir__())
+    # print(obj.__doc__)
     return result
 
 
 if __name__ == '__main__':
     introspection_info('123')
+    introspection_info(123)
+    introspection_info(requests)
