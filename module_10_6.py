d4b3c5167869f65d4b3839a5c94fb4cf17c707c5
Дмитрий Янно <ugor.finno@yandex.ru>
2025-01-16 16:45:21 +0500

    modify module 10 hw 6 Introspection

--- module_10_6.py
+++ module_10_6.py
@@ -1,19 +1,46 @@
 import requests
+import inspect
+from pprint import pprint
 
 
-def introspection_info(obj)->dict:
-    result={}
-    info = dir(obj)
-    if '__name__' in info:
-        print(obj.__name__)
-    print(type(obj))
-    print(info)
-    print(obj.__dir__())
-    # print(obj.__doc__)
+class MyClass:
+    CLASS_VAR = 100
+
+    def __init__(self):
+        self.a = 1
+        self. b = 2
+
+    def my_method(self)->bool:
+        return True
+
+def introspection_info(obj) -> dict:
+    result = {}
+    result['info'] = [el for el in dir(obj) if not el.startswith('_')]
+    if hasattr(obj,'__name__'):
+        result['name'] = obj.__name__
+    # print(type(obj))
+    result['type'] = type(obj)
+    if hasattr(obj,'__dict__'):
+        result['attr'] = [el for el in list(obj.__dict__.keys()) if not el.startswith('_')]
+    result['methods'] = inspect.getmembers(obj,predicate=inspect.isfunction)
+    result['module'] = inspect.getmodule(obj)
+    if hasattr(obj,'__doc__'):
+        result['doc'] = obj.__doc__
+
+    # print(info)
+    # print(obj.__dir__())
+    #print(obj.__doc__)
     return result
 
 
 if __name__ == '__main__':
-    introspection_info('123')
-    introspection_info(123)
-    introspection_info(requests)
+    pprint('String')
+    pprint(introspection_info('123'))
+    pprint('Int')
+    pprint(introspection_info(123))
+    pprint('requests')
+    pprint(introspection_info(requests))
+    pprint('MyClass')
+    my_class = MyClass()
+    pprint(introspection_info(MyClass))
+    pprint(introspection_info(my_class))
